0409e33edabda7b4c0d8302b65f70563aca28eb7
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-02-06 10:31:57 +0530

    Updated total commits

--- src/components/heat-map.tsx
+++ src/components/heat-map.tsx
@@ -2,11 +2,13 @@
 import GitHubCalendar from "react-github-calendar";
 import { Section } from "./ui/section";
 import { useTheme } from "next-themes";
+import { useState } from "react";
 
 const GitHubUsername = "sujiththirumalaisamy";
 
 export default function HeatMap() {
   const { theme } = useTheme();
+  const [totalCommits, setTotalCommits] = useState(0);
   const isLeapYear =
     new Date().getFullYear() % 4 === 0 &&
     (new Date().getFullYear() % 100 !== 0 ||
@@ -18,8 +20,10 @@ export default function HeatMap() {
         hideMonthLabels
         colorScheme={theme && theme === "dark" ? "dark" : "light"}
         transformData={(data) => {
+          setTotalCommits(data.reduce((acc, curr) => acc + curr.count, 0));
           return isLeapYear ? data.slice(79, 366) : data.slice(78, 365);
         }}
+        totalCount={totalCommits}
         weekStart={1}
       />
     </Section>
