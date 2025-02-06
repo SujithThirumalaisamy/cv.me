fbc0d6d8c2740f837db3448ce29a62be6e071f93
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-02-06 10:46:52 +0530

    Updated default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
