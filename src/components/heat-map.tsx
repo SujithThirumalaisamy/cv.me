930d5adcb22293f5b4aba4a9e9004b2da1af1cb9
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-02-11 20:16:00 +0530

    Fixed overflow heatmap

--- src/components/heat-map.tsx
+++ src/components/heat-map.tsx
@@ -24,7 +24,7 @@ export default function HeatMap() {
           return !isLeapYear ? data.slice(79, 366) : data.slice(78, 365);
         }}
         totalCount={totalCommits}
-        weekStart={(new Date().getDay() || 7) - 1 as any}
+        weekStart={(new Date().getDay() || 7) as any}
       />
     </Section>
   );
